005ec24b99d3e2e8322fe8522eea633c3f5b1456
Augusto <senescal.oliveira@gmail.com>
2025-06-13 23:00:15 -0300

    feat: adicao da invencao model

---
+++ src/app/models/invencao-model.ts
@@ -0,0 +1,11 @@
+export interface Invencao {
+    id: number;
+    nome: string;
+    ano: number;
+    inventor: string;
+    categoria: string;
+    imagem: string;
+    descricao: string;
+    destaque: boolean;
+    local: string;
+}
