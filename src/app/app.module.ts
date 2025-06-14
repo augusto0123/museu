6b6206a6793a6a676f7bb5115f362c3560f6cbe1
Augusto <senescal.oliveira@gmail.com>
2025-06-13 22:56:20 -0300

    feat: adicao das paginas do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogoComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
